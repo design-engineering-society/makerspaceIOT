2e7f44e94101ce81d7e13ca205837193ea3279fd
legatro <joshfanderson1@gmail.com>
2019-07-18 14:44:35 +0100

    XMLRequests

--- LED_wifi_control/LED_wifi_control.ino
+++ LED_wifi_control/LED_wifi_control.ino
@@ -67,3 +67,4 @@ void handleLED() {                          // If a POST request is made to URI 
 void handleNotFound(){
   server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
+
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const request = require('request');
+const path = require('path');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!!</h1>");
+    res.sendFile(path.join(__dirname + "/root.html"));
 });
 
 app.get("/test", (req, res) => {
@@ -13,16 +14,43 @@ app.get("/test", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-const options = {  
-    url: 'http://192.168.0.158:80/onOff?plug=1',
-    method: 'GET',
-    headers: {
-        'Accept': 'text/plain',
-        'Accept-Charset': 'utf-8'
-    }
-};
 
-request(options, (err, res, body) => console.log(body));
+function onOff() {
+
+    console.log("onOff");
+    const options = {  
+        url: 'http://192.168.0.158:80/onOff?plug=1',
+        method: 'GET',
+        headers: {
+            'Accept': 'text/plain',
+            'Accept-Charset': 'utf-8'
+        }
+    };
+
+    request(options, (err, res, body) => console.log(body));
+}
+
+function updateESP() {
+
+    console.log("updateESP");
+    const options = {  
+        url: 'http://192.168.0.158:80/update',
+        method: 'POST',
+        headers: {
+            'Accept': 'text/plain',
+            'Accept-Charset': 'utf-8'
+        },
+        json: {
+            "IP": "192.168.0.52",
+            "description": "3D Printer Rack 2",
+            "masterIP": "192.168.0.254",
+            "plug1": "3D Printer 5",
+            "plug2": "3D Printer 6",
+            "plug3": "3D Printer 7",
+            "plug4": "3D Printer 8"
+        }
+    };
+    request(options, (err, res, body) => console.log(body));
+}
