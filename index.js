d17e59df7c37b04a1d338652bf5bedd2d080088f
legatro <joshfanderson1@gmail.com>
2019-07-24 10:52:20 +0100

    testing with mongoDB

--- index.js
+++ index.js
@@ -19,44 +19,11 @@ app.get("/init", (req, res) => {
     res.status(200).send('Init request recieved');
 });
 
+app.get("/reconnect", (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`Recieved a reconnect request from device with IP: ${ip}`);
+    res.status(200).send('Reconnect request recieved');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-/*function onOff() {
-
-    console.log("onOff");
-    const options = {  
-        url: 'http://192.168.0.158:80/onOff?plug=1',
-        method: 'GET',
-        headers: {
-            'Accept': 'text/plain',
-            'Accept-Charset': 'utf-8'
-        }
-    };
-
-    request(options, (err, res, body) => console.log(body));
-}
-
-function updateESP() {
-
-    console.log("updateESP");
-    const options = {  
-        url: 'http://192.168.0.158:80/update',
-        method: 'POST',
-        headers: {
-            'Accept': 'text/plain',
-            'Accept-Charset': 'utf-8'
-        },
-        json: {
-            "IP": "192.168.0.52",
-            "description": "3D Printer Rack 2",
-            "masterIP": "192.168.0.254",
-            "plug1": "3D Printer 5",
-            "plug2": "3D Printer 6",
-            "plug3": "3D Printer 7",
-            "plug4": "3D Printer 8"
-        }
-    };
-    request(options, (err, res, body) => console.log(body));
-}*/
