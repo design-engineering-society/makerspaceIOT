826d9601add3135a9b4badbf727514337f9e023b
Michael Hofmann <michael@dyn3177-114.wlan.ic.ac.uk>
2019-09-04 15:18:53 +0100

    changed IP from Josh's Laptop to Michael's Mac

--- Device-Master/index.js
+++ Device-Master/index.js
@@ -8,7 +8,7 @@ const dbUtil = require('./dbUtil.js');
 const util = require('./util.js');
 
 const routerIP = "192.168.0.254";
-const masterIP = "192.168.0.110"; //ip of josh laptop
+const masterIP = "192.168.0.160"; //ip of Michael's Mac
 const ssid = "TP-Link_6F62";
 const password = "78059757";
 
